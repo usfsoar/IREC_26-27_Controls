b50d278e5bd3a39e060f3877dae3f0045ba04e51
Matthew <matthewjwest53@gmail.com>
2026-09-24 20:42:37 -0400

    Initial structure created

---
+++ init/src/main.cpp
@@ -0,0 +1,19 @@
+
+
+
+int main(int argc, char *argv[]) 
+{
+    // Check for power loss
+
+
+    // Perform any general hardware initialization
+
+
+    // Start threads
+
+
+    // Block on threads
+        // If any threads end without pass, handle
+
+    return 0;
+}
